d4d8189cb52faee488a8de09f6ce914a4a5c7bb2
ramam <ramamatrea@yahoo.com>
2023-01-09 18:50:21 +1100

    update

--- assets/js/test3.js
+++ assets/js/test3.js
@@ -14,7 +14,7 @@ recipeCloseBtn.addEventListener('click', () => {
 function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     console.log(searchInputTxt.length);
-    fetch(`https://api.spoonacular.com/recipes/search?apiKey=d03a24375b30427a863ee13274b2ca29&number=6&query=${searchInputTxt}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response=>response.json())
     .then(data => {
         let html = " ";
@@ -47,7 +47,7 @@ function getMealRecipe(e){
     e.preventDefault();
     if(e.target.classList.contains('recipe-btn')){
         let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://api.spoonacular.com/recipes/search?apiKey=d03a24375b30427a863ee13274b2ca29&number=6&query=${mealItem.dataset.id}`)
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
         .then(response => response.json())
         .then(data => mealRecipeModal(data.meals));
     }
